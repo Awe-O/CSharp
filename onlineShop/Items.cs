b9c94db58a4bf05181d649d84fb94e16e86a30e6
Awe <dmitry.shkenev@gmail.com>
2020-02-08 18:29:58 +0600

    changed gitignore, added onlineShop project

---
+++ onlineShop/Items.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onlineShop
+{
+    public class Items
+    {
+        private int _id;
+        private string _name;
+        private string _description;
+        private string _coulor;
+        private decimal _price;
+        private bool _availability;
+        private int _itemsAvailable;
+
+        public Items()
+        {
+            
+        }
+
+        public Items(int id)
+        {
+            _id = id;
+        }
+
+        public Items(int id, string name, decimal price)
+            : this(id)
+        {            
+            _name = name;
+            _price = price;
+        }
+
+        public Items(int id, string name, decimal price, 
+            bool availability, int itemsAvailable)
+            : this(id, name, price)
+        {
+            _availability = availability;
+            _itemsAvailable = itemsAvailable;
+        }
+
+        public Items(int id, string name, decimal price,
+            bool availability, int itemsAvailible, 
+            string description, string coulor)
+            : this(id, name, price, availability, itemsAvailible)
+        {
+            _description = description;
+            _coulor = coulor;
+        }
+
+        public int Id { get { return _id; } }
+        public string Name { get { return _name; } }
+        public string Description { get { return _description; } }
+        public string Colour { get { return _coulor; } }
+        public decimal Price { get { return _price; } }
+        public bool Availability { get { return _availability; } }
+        public int ItemsAbailable { get { return _itemsAvailable; } }
+    }
+}
---
+++ onlineShop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onlineShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ketler = new Items(
+                1, "Ketler", 2499.00m);
+            var vacuumCleaner = new Items(
+                2, "Vaccum cleaner", 15999.00m);
+            var iron = new Items(
+                5, "Iron", 999.00m);
+            var microwave = new Items(
+                10, "Microwave", 4499.00m, true, 3);
+            var laptop = new Items(
+                6, "Laptop", 49999.00m, 
+                false, 0, "The best laptop ever!", "black");
+            var hairDyier = new Items(
+                9, "Hair dryer", 1499.00m,
+                true, 10, "Your hair always should be dry!", "red");
+
+            var itemsInStock = new List<object>
+            { ketler, vacuumCleaner, iron, microwave, laptop, hairDyier };
+
+
+            Console.WriteLine($"Items in stock now = {itemsInStock.Count}. \r\n" +
+                $"Items: {ketler.Name}, {vacuumCleaner.Name}, {iron.Name}, {microwave.Name}, {laptop.Name}, {hairDyier.Name}.");
+
+        }
+    }
+}
